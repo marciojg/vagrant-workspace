6b44fa7e44127887a3324262ee275e3cd79d705f
Marcio de Jesus Gonçalves da Silva <MJesus@lbv.org.br>
2020-01-14 11:28:38 -0300

    First commit

---
+++ vagrant.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Basic update.
+apt-get update -y
+apt-get dist-upgrade -y
+
+# Basic dependencies.
+apt-get install -y wget dpkg sed curl git zip unzip ssh vim w3m
+
+# GUI.
+apt-get install -y lubuntu-desktop
+
+# Google Chrome.
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt-get install -f -y
+
+# Visual Studio Code.
+wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+dpkg -i vscode.deb
+apt-get install -f -y
+
+# Docker.
+apt-get install -y containerd
+apt-get install -y docker.io
+
+# Docker compose.
+apt-get install -y docker-compose
+
+# VirtualBox Guest Additions.
+apt-get install -y virtualbox-guest-additions-iso
+apt-get install -y virtualbox-guest-dkms
+apt-get install -y virtualbox-guest-utils
+apt-get install -y virtualbox-guest-x11
+
+# Timezone.
+echo "sudo timedatectl set-timezone America/Sao_Paulo" >> /home/vagrant/.profile
+
+# Keyboard.
+sed -i "/XKBMODEL=/c\XKBMODEL=\"abnt2\"" /etc/default/keyboard
+sed -i "/XKBLAYOUT=/c\XKBLAYOUT=\"br\"" /etc/default/keyboard
+echo "sudo setxkbmap -model abnt2 -layout br" >> /home/vagrant/.profile
+echo "sudo setxkbmap -model abnt2 -layout br" >> /home/vagrant/.bashrc
+
+# Inotify - Increasing the amount of inotify watchers (https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers).
+echo "echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p" >> /home/vagrant/.profile
+
+echo "Vagrant workspace created!"
